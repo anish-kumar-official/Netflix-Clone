b3f7119bbc109c00de81e7171797b4660d2c979f
Kumar <Anish.Kumar@insight.com>
2023-09-01 17:14:32 +0530

    signup updated

--- src/Pages/Auth/Signup.jsx
+++ src/Pages/Auth/Signup.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
@@ -10,9 +11,12 @@ const Signup = () => {
       />
       <div className="bg-black/60 top-0 left-0 z-10 w-full h-screen fixed flex justify-center items-center"></div>
       <div className="w-full h-screen flex justify-center items-center py-[30px] px-5">
-        <div className="bg-black/60 w-[450px] h-[400px] z-10 mt-5 flex flex-col px-[30px] py-[25px] gap-5">
-          <div className="name text-white text-[30px] px-10">Signup</div>
-          <div className="inputs flex flex-col gap-3 justify-center items-center">
+        <div className="bg-black/60 w-[450px] h-[400px] z-10 flex flex-col px-[30px] py-[25px] gap-5 items-center">
+          <div className="name text-white text-[30px] px-10 w-full">
+            Sign Up
+          </div>
+
+          <div className="inputs flex flex-col gap-3 justify-center items-center w-full">
             <input
               type="email"
               className="w-[75%] px-[15px] py-[10px] rounded bg-gray-600/50 text-white"
@@ -28,10 +32,17 @@ const Signup = () => {
               className="w-[75%] px-[15px] py-[10px] rounded  bg-gray-600/50 text-white"
               placeholder="Confirm Password"
             />
+            <button className="btnSignin rounded bg-red-600 px-[15px] py-[10px] mt-3 text-white w-[75%] hover:bg-red-700">
+              Sign Up
+            </button>
           </div>
-          <button className="btnSignin rounded bg-red-600 px-[15px] py-[10px] mt-3 text-white w-[75%] ml-12 hover:bg-red-800">
-            Signup
-          </button>
+
+          <div className="flex justify-between mt-3 text-white">
+            Already a user ?{" "}
+            <Link to="/login" className="text-white ml-3">
+              Sign In
+            </Link>
+          </div>
         </div>
       </div>
     </div>
